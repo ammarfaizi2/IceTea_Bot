fea75d0ab6183d11679f51472acbaf1cc4dd379e
U0_a147 <ammarfaizi2@gmail.com>
2017-04-29 19:31:48 +0700

    Fix token

--- webhook.php
+++ webhook.php
@@ -4,5 +4,5 @@ is_dir("data") or mkdir("data");
 require __DIR__."/loader.php";
 $wb = json_decode(file_get_contents("php://input"),true);
 #print_r($wb);
-$app = new mgmt($wb,new Telegram("348646582:AAGjRQ6eW-WjVqInD_rwwocMjy3Kk--Rblg"));
+$app = new mgmt($wb,new Telegram("308645660:AAG-EIkc2qgXsJ2zRk8A4fAREOKXUxKuxM8"));
 $app->run();
