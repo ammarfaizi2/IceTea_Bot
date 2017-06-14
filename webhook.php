9d72ab092f8a9689834d4d981a7d8831f4cf5f4d
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-14 09:44:46 +0700

    Update

--- webhook.php
+++ webhook.php
@@ -3,6 +3,7 @@ header("Content-type:application/json");
 is_dir("data") or mkdir("data");
 require __DIR__."/loader.php";
 $wb = json_decode(file_get_contents("php://input"),true);
+#file_put_contents("text.txt", json_encode($wb,128));
 #print_r($wb);
-$app = new mgmt($wb,new Telegram("Token disini"));
+$app = new mgmt($wb,new Telegram("308645660:AAG-EIkc2qgXsJ2zRk8A4fAREOKXUxKuxM8"));
 $app->run();
